3ec370865e8c74c24b4a353f09e3eaafd91cfaa0
503 <503@academicos.cesde.edu.co>
2021-09-17 08:52:31 -0500

    Se crea menu

--- src/empresa/Empresa.java
+++ src/empresa/Empresa.java
@@ -1,12 +1,32 @@
 
 package empresa;
 
+import java.util.Scanner;
+
 
 public class Empresa {
 
     
     public static void main(String[] args) {
-        
+       Scanner entrada = new Scanner(System.in);
+       int opcion;
+       
+       do{
+           System.out.println("Bienvenido");
+           System.out.println("---------------------");
+           System.out.println("1.Agregar empleado");
+           System.out.println("2.Mostrar empleado");
+           System.out.println("3.Mostar empleados");
+           System.out.println("4.Salir");
+           System.out.println("---------------------");
+           System.out.println("");
+           
+           System.out.println("Digite una opcion");
+           opcion = entrada.nextInt();
+           
+           
+       }while(opcion != 4);
+       
        
     }
     
