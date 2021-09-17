ea3ec058470a869df57803ab5496f05fc15a3198
503 <503@academicos.cesde.edu.co>
2021-09-17 08:34:48 -0500

    se crea clase clase empleado

---
+++ src/empleado/Empleado.java
@@ -0,0 +1,46 @@
+
+package empleado;
+
+/**
+ *
+ * @author 503
+ */
+public class Empleado {
+    // atributois
+    int id;
+    String nombre;
+    String cargo;
+    int salario;
+    String telefono;
+    String direccion;
+    
+    
+    // metodo constructor
+           
+    public Empleado() {
+    }
+
+    public Empleado(int id, String nombre, String cargo, int salario, String telefono, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+       
+    
+    }
+     // metodos o funciones
+    public int calcularSalario(){
+        return(0);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
